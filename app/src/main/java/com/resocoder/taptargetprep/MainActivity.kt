b1b3b6bf1b657df1d05d9fc9512502d964f345a2
Matej Resetar <mresoo99@gmail.com>
2017-12-11 19:25:49 +0100

    Add project files.

---
+++ app/src/main/java/com/resocoder/taptargetprep/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.resocoder.taptargetprep
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.preference.PreferenceManager
+import kotlinx.android.synthetic.main.activity_main.*
+import uk.co.samuelwall.materialtaptargetprompt.MaterialTapTargetPrompt
+import uk.co.samuelwall.materialtaptargetprompt.extras.backgrounds.RectanglePromptBackground
+import uk.co.samuelwall.materialtaptargetprompt.extras.focals.RectanglePromptFocal
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        showFabPrompt()
+    }
+
+    private fun showFabPrompt(){
+        val prefManager = PreferenceManager.getDefaultSharedPreferences(this)
+
+        if (!prefManager.getBoolean("didShowPrompt", false)){
+            MaterialTapTargetPrompt.Builder(this)
+                    .setTarget(fab)
+                    .setPrimaryText("Click me!")
+                    .setSecondaryText("I'm a floating action button aka FAB.")
+                    .setBackButtonDismissEnabled(true)
+                    .setPromptStateChangeListener { prompt, state ->
+                        if (state == MaterialTapTargetPrompt.STATE_FOCAL_PRESSED
+                                || state == MaterialTapTargetPrompt.STATE_NON_FOCAL_PRESSED){
+                            val prefEditor = prefManager.edit()
+                            prefEditor.putBoolean("didShowPrompt", true)
+                            prefEditor.apply()
+
+                            showButtonPrompt()
+                        }
+                    }
+                    .show()
+        }
+    }
+
+    private fun showButtonPrompt(){
+        MaterialTapTargetPrompt.Builder(this)
+                .setTarget(button)
+                .setPrimaryText("Press me!")
+                .setSecondaryText("I'm a nice and simple button.")
+                .setBackButtonDismissEnabled(true)
+                .setPromptBackground(RectanglePromptBackground())
+                .setPromptFocal(RectanglePromptFocal())
+                .show()
+    }
+}
